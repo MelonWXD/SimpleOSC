7fde237369b782b4476290a099382031675b5dbd
Lewis <a549295838@163.com>
2017-12-15 21:03:16 +0800

    PageBean

--- app/src/main/java/com/dongua/simpleosc/bean/PageBean.java
+++ app/src/main/java/com/dongua/simpleosc/bean/PageBean.java
@@ -2,11 +2,13 @@ package com.dongua.simpleosc.bean;
 
 import android.os.Parcelable;
 
+import java.io.Serializable;
+import java.util.List;
+
 /**
  * 新闻Fragment的bean基类，提供公共方法来支持泛型，实现代码复用
  */
 
-public interface INewsBean extends Parcelable{
-    public String getPubDate();
-    public long getPubDateLong();
+public abstract class PageBean<T> implements Serializable{
+    private List<T> items;
 }
--- app/src/main/java/com/dongua/simpleosc/bean/PostBean.java
+++ app/src/main/java/com/dongua/simpleosc/bean/PostBean.java
@@ -2,11 +2,15 @@
 package com.dongua.simpleosc.bean;
 
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import org.greenrobot.greendao.annotation.Entity;
 import org.greenrobot.greendao.annotation.Id;
 import org.greenrobot.greendao.annotation.Unique;
+
 @Entity
-public class PostBean implements INewsBean {
+public class PostBean implements Parcelable {
 
     @Id(autoincrement = true)
     private Long dbID;
@@ -24,16 +28,78 @@ public class PostBean implements INewsBean {
     private String answerName;
     private String answerTime;
 
-    @Override
+
     public String getPubDate() {
         return pubDate;
     }
 
 
-    @Override
+
     public long getPubDateLong() {
         return this.pubDateLong;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    //    private int id;
+//    private String author;
+//    private String pubDate;
+//    private long pubDateLong;
+//    private int authorid;
+//    private String portrait;
+//    private String title;
+//    private long viewCount;
+//
+//    private long answerCount;
+//    private String answerName;
+//    private String answerTime;
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(id);
+        parcel.writeString(author);
+        parcel.writeString(pubDate);
+        parcel.writeLong(pubDateLong);
+        parcel.writeInt(authorid);
+        parcel.writeString(portrait);
+        parcel.writeString(title);
+        parcel.writeLong(viewCount);
+        parcel.writeLong(answerCount);
+        parcel.writeString(answerName);
+        parcel.writeString(answerTime);
+
+    }
+
+
+    public PostBean(Parcel parcel) {
+        this.id = parcel.readInt();
+        this.author = parcel.readString();
+        this.pubDate = parcel.readString();
+        this.pubDateLong = parcel.readLong();
+        this.title = parcel.readString();
+        this.authorid = parcel.readInt();
+        this.portrait = parcel.readString();
+        this.title = parcel.readString();
+        this.viewCount = parcel.readLong();
+        this.answerCount = parcel.readLong();
+        this.answerName = parcel.readString();
+        this.answerTime = parcel.readString();
+
+    }
+
+    public static final Creator<PostBean> CREATOR = new Creator<PostBean>() {
+        @Override
+        public PostBean createFromParcel(Parcel parcel) {
+            return new PostBean(parcel);
+        }
+
+        @Override
+        public PostBean[] newArray(int i) {
+            return new PostBean[i];
+        }
+    };
 
 
 }
