439c8fb953ec244aa99f8a6c7282f970ed818db9
Lewis <a549295838@163.com>
2018-01-02 20:02:35 +0800

    封装Fragment懒加载

---
+++ app/src/main/java/com/dongua/simpleosc/activity/DetailActivity.java
@@ -0,0 +1,23 @@
+package com.dongua.simpleosc.activity;
+
+import android.support.v7.widget.Toolbar;
+
+import com.dongua.simpleosc.base.activity.BaseToolBarActivity;
+import com.dongua.simpleosc.base.fragment.BaseToolBarFragment;
+
+/**
+ * Created by duoyi on 17-12-23.
+ */
+
+public class DetailActivity extends BaseToolBarActivity {
+
+    @Override
+    protected int getLayoutID() {
+        return 0;
+    }
+
+    @Override
+    protected void setCustomToolbar(Toolbar toolbar) {
+
+    }
+}
---
+++ app/src/main/java/com/dongua/simpleosc/base/fragment/BaseLazyFragment.java
@@ -0,0 +1,76 @@
+package com.dongua.simpleosc.base.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by duoyi on 17-12-29.
+ */
+
+public abstract class BaseLazyFragment extends BaseFragment {
+
+    protected boolean isVisible;
+    private boolean isPrepared;
+    private boolean isFirst = true;
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        isPrepared = true;
+
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if(getUserVisibleHint()){
+            isVisible = true;
+            lazyLoad();
+        }else{
+            isVisible = false;
+            onInvisible();
+        }
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(getUserVisibleHint()){
+            setUserVisibleHint(true);
+        }
+    }
+
+
+    protected void lazyLoad(){
+        if(!isPrepared || !isVisible || !isFirst){
+            return;
+        }
+        initWidget(mRoot);
+        initData();
+
+        isFirst = false;
+    }
+
+
+    protected void onInvisible() {
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        mRoot = inflater.inflate(getLayoutId(), container, false);
+        mUnbinder = ButterKnife.bind(this, mRoot);
+        mRoot = initRootView(mRoot);
+
+        return mRoot;
+    }
+}
